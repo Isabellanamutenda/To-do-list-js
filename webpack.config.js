13582b401f3b6211319b15020b1f24970f048d7b
Isabellanamutenda <namtendaisabella@.com>
2021-08-31 10:53:23 +0300

    Add pages

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
+    publicPath: "/To-do-list-js",
   },
   module: {
     rules: [
