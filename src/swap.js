2c8ea9db748f88bf77e3d464af9fdf5ceee95ed8
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-11-02 13:24:26 +0100

    Add some space to swap.js file

--- src/swap.js
+++ src/swap.js
@@ -3,6 +3,8 @@ import { setStorage } from './storage.js';
 
 export default (arr, index1, index2) => {
   [arr[index1].description, arr[index2].description] = [arr[index2].description, arr[index1].description];
+
+  
   [arr[index1].completed, arr[index2].completed] = [arr[index2].completed, arr[index1].completed];
   setStorage(arr);
   return arr;
